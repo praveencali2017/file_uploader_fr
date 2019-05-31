3f374838e5a6bc1fbfda8dabf7da1d543a786830
Prav <praveenknjun12@gmail.com>
2019-05-30 18:04:47 -0700

    added datasource and its corresponding functionalities

--- uploader_back_end/controller.py
+++ uploader_back_end/controller.py
@@ -3,41 +3,65 @@ from config.default import *
 from utils import validate
 from utils.search import search
 from werkzeug.utils import secure_filename
+from utils.data_constructor import build_data_to_insert
 import json
 app = Flask(__name__)
 app.secret_key =SECRET_KEY
 app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
 
-
+"""
+Index page
+"""
 @app.route('/')
 def index_upload():
     return render_template('index.html')
 
+
+"""
+Serves file upload
+"""
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
+    """
+    Uploads file to the system and stores the data into the db, later deletes the file on success
+    :return: JSON response
+    """
     if request.method=='POST':
         file = request.files['file']
         if file and validate.check_file(file.filename):
             file_name = secure_filename(file.filename)
             path=os.path.join(app.config['UPLOAD_FOLDER'], file_name)
             file.save(path)
-            return jsonify({"msg":"File Uploaded!!!"}), 200
+            is_success=build_data_to_insert(path)
+            if is_success:
+               os.remove(path)
+               return jsonify({"msg":"File Uploaded!!!"}), 200
+            else:
+               return jsonify({"msg": "Upload Failed!!! Unable to store"}), 500
         else:
             return jsonify({"msg":"Upload Failed!!! check file type (only csv file types are supported)"}), 500
     return 
 
+
+"""
+Serves search functionality
+"""
 @app.route('/search', methods=['GET', 'POST'])
 def search_key():
+    """
+    Extracts key (input) from query string and finds the relevant value from the db, id any.
+    :return: JSON response
+    """
     if request.method == 'GET':
         key=request.args.get('key_txt')
         if key:
             result=search(key)
-            if result:
-                return jsonify({"result":result}), 200
-            else:
-                return
-    return
+            return jsonify({"result":result}), 200
+    return jsonify({"result":[]}), 200
 
 
+"""
+Main Block
+"""
 if __name__ =='__main__':
     app.run(HOST,PORT)
---
+++ uploader_back_end/utils/db_helper.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from ..config.default import DB_HOST
+engine = create_engine(DB_HOST, echo = True)
+Base = declarative_base()
+Session= sessionmaker(bind=engine)
+
+"""
+Deals with DB specifics operations
+"""
+class TagInfo(Base):
+    __tablename__='taginfo'
+    id = Column(Integer, primary_key=True)
+    key = Column(String)
+    value = Column(String)
+
+
+Base.metadata.create_all(engine)
+
+def insert_data(records):
+    """
+    Inserts all the field values as bulk insert
+    :param records: {keys, values}
+    :return: 1 and 0, 1 for success and 0 for failure
+    """
+    session = Session()
+    try:
+        result=[]
+        for key, value in records.items():
+            info = TagInfo(key=key, value=value)
+            result.append(info)
+        session.add_all(result)
+        session.commit()
+        return 1
+    except Exception as e:
+        session.rollback()
+        return 0
+
+
+def select_result(key):
+    """
+    Fetches data based on the given key
+    :param key:
+    :return: list of values
+    """
+    session = Session()
+    resultset=session.query(TagInfo.value).filter(TagInfo.key.ilike(key)).all()
+    return [value for (value,) in resultset]
+
+
